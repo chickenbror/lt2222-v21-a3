66f0146ec4973c8dd6a06af485c8a5d73952cfda
chickenbror <75183404+chickenbror@users.noreply.github.com>
2021-03-29 17:02:36 +0200

    saving part1

--- README.md
+++ README.md
@@ -4,6 +4,36 @@ Your name:
 
 ## Part 1
 
+#### a(f):
+Reads a text file and returns a two-value tuple: a list of char-tokens & a list of non-repeating char-tokens (ie the vocab-list).
+Arg: 
+    f: string representing the filepath/filename.
+Returns:
+    The abovementioned tuple of two lists. List1 has doubled start/end '<s>' and '<e>' tokens plus all char-tokens, whereas the List2 has single start/end tokens plus non-repeating char-tokens (ie the vocab-list).
+
+#### g(x,p):
+Takes a token and a vocab-list, and returns a numpy array that represents a one-hot vector.
+Args: 
+    x: a char-token
+    p: a vocab-list which includes x
+Returns: 
+    The one-hot vector of x (in the form of a numpy array).
+
+#### b(u,p): 
+Loops over every char-token returned by a() and looks for vowels. Returns a list of found vowel-indexes and a list of the former's corresponding context-features.
+Args: 
+    u: a list of char-tokens which function a(textfile) returned
+    p: a list of non-repeating char-tokens which function a(textfile) returned (ie, the vocab of textfile, in the sense that each vocab is a char )
+Returns: 
+    Two lists of the same length. List1 has all the vowels found in u, each of which is mapped to the corresponding index in 'vowels' list. List2 is a list of numpy arrays, each of which correspond to a vowel from List1, and is the concatenation of 4 one-hot vectors (of the 2 tokens before/after the vowel-token).
+
+#### CLI args:
+    m: the text file path
+    h: destination to save the trained model
+    --k: hiddensize (parameter of train(); 200 by default if left blank )
+    --r: epochs (parameter of train(); 100 by default if left blank )
+
+
 ## Part 2
 
 ## Part 3
--- train.py
+++ train.py
@@ -15,30 +15,31 @@ vowels = sorted(['y', 'é', 'ö', 'a', 'i', 'å', 'u', 'ä', 'e', 'o'])
 def a(f):
     mm = []
     with open(f, "r") as q:
-        for l in q:
-            mm += [c for c in l]
+        for l in q:  #for each line in the opened file
+            mm += [c for c in l] #extend each line's character-tokens to mm 
 
-    mm = ["<s>", "<s>"] + mm + ["<e>", "<e>"]
-    return mm, list(set(mm))
+    mm = ["<s>", "<s>"] + mm + ["<e>", "<e>"]  # add start/end tokens
+    return mm, list(set(mm)) #two values: mm  with or without repeated char-tokens
 
-def g(x, p):
-    z = np.zeros(len(p))
-    z[p.index(x)] = 1
-    return z
+def g(x, p): # single token & list of unique tokens
+    z = np.zeros(len(p)) # array of 0; len of array==nr of unique tokens
+    z[p.index(x)] = 1  # change the nth 0 that has the same index of x in p
+    return z # np array representing a x's one-hot vector
 
-def b(u, p):
+def b(u, p): #char-tokens list; unique char-tokens list
     gt = []
     gr = []
-    for v in range(len(u) - 4):
-        if u[v+2] not in vowels:
-            continue
+    for v in range(len(u) - 4): #indexing 0 ~ -5 => ie, after adding 2 >> from first token to last token, excl the <s>/<e>
+        if u[v+2] not in vowels: # if the char-token (indexed 2 ~ -3) isn't a vowel
+            continue             # don't run the next 4 lines, and instead loop the next char-token
         
-        h2 = vowels.index(u[v+2])
-        gt.append(h2)
-        r = np.concatenate([g(x, p) for x in [u[v], u[v+1], u[v+3], u[v+4]]])
-        gr.append(r)
+        #(Below only applies if char-token is a vowel)
+        h2 = vowels.index(u[v+2]) # encode the char-token to vowels-list idx
+        gt.append(h2)  # gt=list of vowel idxes
+        r = np.concatenate([g(x, p) for x in [u[v], u[v+1], u[v+3], u[v+4]]]) # concatenated array of one-hot vectors of 4 neighbouring tokens (2 before, 2 after)
+        gr.append(r) # list of context-features (ie, concatenation of 4 neighbouring one-hot vectors)
 
-    return np.array(gr), np.array(gt)
+    return np.array(gr), np.array(gt) # list of indexed vowels, list of concatenated one-hot vectors
         
 
 if __name__ == "__main__":
@@ -47,11 +48,12 @@ if __name__ == "__main__":
     parser.add_argument("--r", dest="r", type=int, default=100)
     parser.add_argument("m", type=str)
     parser.add_argument("h", type=str)
+    # add_argument(args...)
     
     args = parser.parse_args()
 
-    q = a(args.m)
-    w = b(q[0], q[1])
-    t = train(w[0], w[1], q[1], args.k, args.r)
+    q = a(args.m) # tokenise => tokens list, vocab list
+    w = b(q[0], q[1]) # find vowels from tokens list => list of indexed vowels, list of each vowel's context-features
+    t = train(w[0], w[1], q[1], args.k, args.r) # vowels, features, vocab, hiddensize, epochs
 
-    torch.save(t, args.h)
+    torch.save(t, args.h) # save trained model to destination
